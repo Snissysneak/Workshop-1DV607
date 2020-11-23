5d43db949a8fa7b0390546355e805f8a2ed1a8f5
bmnetsec <70882434+bmnetsec@users.noreply.github.com>
2020-11-23 17:55:43 +0100

    view changed

--- Workshop 2/Final Version/Source code/view/RegistryView.java
+++ Workshop 2/Final Version/Source code/view/RegistryView.java
@@ -2,74 +2,215 @@ package view;
 import model.Boat;
 import model.Member;
 import model.MemberRegistry;
-
 import java.util.Scanner;
 
 public class RegistryView {
     protected MemberRegistry rv_memberRegistry = new MemberRegistry();
-    protected Member rv_member = new Member();
+    private final static Scanner sc = new Scanner(System.in);
 
+    //*******************Member*****************************************
 
-    /*Check if record with given member exist and send data to printMember()*/
-    public void getMemberInfo(int memberID) {
+    /* Create member process with instructions */
+    public void createMemberView() {
+        Member rv_member = new Member();
+        System.out.print("Set name: ");
+        rv_member.setName(sc.next());
+        System.out.print("Set personal number (10 numbers): ");
+        String v_personalNum = sc.next();
+        personalNumberCheck(v_personalNum);
+        rv_member.setPersonalNum(v_personalNum);
+        int v_memberID = rv_memberRegistry.idGenerator(); //call for rand id
+        rv_member.setMemberID(v_memberID);
+        System.out.println("Generated ID for this member: " + v_memberID);
+        rv_memberRegistry.addMember(rv_member); //call for adding the member
+        System.out.println("Member succesfully created");
+    }
+
+    /* Member info process with instructions */
+    public void getMemberInfoView() {
+        System.out.print("Give id of the member you want to check: ");
+        int id = sc.nextInt();
+        while (existMemberView(id)) {
+            id = sc.nextInt();
+        }
         rv_memberRegistry.readFile();
-        rv_member = rv_memberRegistry.getMember(memberID);
+        Member rv_member = rv_memberRegistry.getMember(id);
         printMember(rv_member);
     }
-    /*Check if given boat exist and send data to printBoat()*/
-    public void getBoatInfo(int a_boatID) {
-        rv_memberRegistry.readFile();
-        for (Member m : rv_memberRegistry.members_list)
-            for(Boat b : m.ownedBoats)
-                if(b.getBoatID() == a_boatID)
-                    printBoat(b);
-    }
-    /*Display information about change member process*/
-    public void changeMemberRegistryView(int a_memberID){
-        rv_memberRegistry.readFile();
-        Scanner sc  = new Scanner(System.in);
 
-        rv_member = rv_memberRegistry.getMember(a_memberID);
-        rv_member.setMemberID(a_memberID);
-
+    /* Change member process with instructions */
+    public void changeMemberView() {
+        System.out.print("Give id of the member you want to change: ");
+        int id = sc.nextInt();
+        while (existMemberView(id)) {
+            id = sc.nextInt();
+        }
+        rv_memberRegistry.readFile();
+        Member rv_member = rv_memberRegistry.getMember(id);
+        rv_member.setMemberID(id);
         System.out.print("Set new name: ");
         rv_member.setName(sc.next());
-
         System.out.print("Set new personal number: ");
         String newPersonNumber = sc.next();
         personalNumberCheck(newPersonNumber);
         rv_member.setPersonalNum(newPersonNumber);
-
         rv_memberRegistry.changeMember(rv_member);
+        System.out.println("Member succesfully changed");
     }
-    /*Display information about change boat process*/
-    public void changeBoatRegistryView(int boatID){
+
+    /* Delete member process with instructions */
+    public void deleteMemberView() {
+        Member rv_member;
+        System.out.print("Give id of the member you want to delete: ");
+        int id = sc.nextInt();
+        while (existMemberView(id)) {
+            id = sc.nextInt();
+        }
+        rv_member = rv_memberRegistry.getMember(id);
+        rv_memberRegistry.deleteMember(rv_member);
+        System.out.println("Member removed!");
+    }
+
+    /*Returns true when memberID is already in file */
+    public boolean existMemberView(int a_memberID) {
+        if (rv_memberRegistry.notExistInFile(a_memberID)) {
+            System.out.print("No such member! Please try again: ");
+        }
+        return rv_memberRegistry.notExistInFile(a_memberID);
+    }
+    /* Method checks person number */
+    public void personalNumberCheck(String a_pn) {
+        while (!rv_memberRegistry.checkPersonalNumber(a_pn)) {
+            System.out.println("Wrong input try again");
+            a_pn = sc.next();
+        }
+
+        //Check if the input already exist in the file
+        while (!rv_memberRegistry.notExistInFile(Integer.parseInt(a_pn))) {
+            System.out.println("Person number: " + a_pn + " already exist in database");
+            a_pn = sc.next();
+        }
+    }
+    //
+    //Boat
+    //
+    public void registerBoatView(){
+        Boat v_boat = new Boat();
+        System.out.println("1 - Sailboat, 2 - Motorsailer, 3 - Kayak/Canoe, 4 - Other");
+        System.out.print("Choose type: ");
+        int type = sc.nextInt();
+        Boat.Type v_boatType = boatTypeView(type);
+        v_boat.setBoatType(v_boatType);
+        System.out.print("Enter boat length: ");
+        String input = sc.next();
+
+        try {
+            int length = Integer.parseInt(input);
+            v_boat.setBoatLength(length);
+        } catch (NumberFormatException ex) {
+            System.out.println("\nWrong input! Please try again");
+        }
+        System.out.print("To what member do you want to add the boat to? (enter Member ID: ");
+        //Verify given ID
+        int v_ownerID = sc.nextInt();
+        while (existMemberView(v_ownerID)) {
+            v_ownerID = sc.nextInt();
+        }
+
+        int v_boatID = rv_memberRegistry.idGenerator();
+        while (rv_memberRegistry.notExistInFile(v_boatID)) {
+            v_boatID = rv_memberRegistry.idGenerator(); //call for rand id
+        }
+        v_boat.setBoatID(v_boatID);
+        System.out.println("Generated ID for this boat: " + v_boatID);
+
+        rv_memberRegistry.addBoat(v_ownerID, v_boat);
+    }
+
+    /* Boat info process with instructions */
+    public void getBoatInfo() {
+        rv_memberRegistry.readFile();
+        System.out.print("Give id of the boat you want to check: ");
+        int id = sc.nextInt();
+        //Verify given ID
+        while (verifyBoatID(id)) {
+            id = sc.nextInt();
+        }
+        for (Member m : rv_memberRegistry.members_list)
+            for(Boat b : m.ownedBoats)
+                if(b.getBoatID() == id)
+                    printBoat(b);
+    }
+    /* Change boat process with instructions */
+    public void changeBoatRegistryView(){
         Boat rv_boat = new Boat();
         rv_memberRegistry.readFile();
-        rv_boat.setBoatID(boatID);
 
-        Scanner scan = new Scanner(System.in);
+        System.out.print("Give id of the boat you want to change: ");
+        int id = sc.nextInt();
+        rv_boat.setBoatID(id);
 
+        while (verifyBoatID(id)) {
+            id = sc.nextInt();
+        }
         System.out.println("1 - Sailboat, 2 - Motorsailer, 3 - Kayak/Canoe, 4 - Other");
         System.out.print("Choose new type: ");
         try {
-            Boat.Type rv_boatType = boatTypeView(scan.nextInt());
+            Boat.Type rv_boatType = boatTypeView(sc.nextInt());
             rv_boat.setBoatType(rv_boatType);
 
         } catch (NumberFormatException ex) {
             System.out.println("\nWrong input! Please try again");
-            changeBoatRegistryView(boatID);
+            changeBoatRegistryView();
         }
         System.out.print("Set new length: ");
         try {
-            int uppdatedLength = scan.nextInt();
+            int uppdatedLength = sc.nextInt();
             rv_boat.setBoatLength(uppdatedLength);
         } catch (NumberFormatException ex) {
             System.out.println("\nWrong input! Please try again");
-            changeBoatRegistryView(boatID);
+            changeBoatRegistryView();
         }
         rv_memberRegistry.changeBoat(rv_boat);
+        System.out.println("Boat succesfully changed");
     }
+    /* Delete boat process with instructions */
+    public void deleteBoatView(){
+        System.out.print("What boat do you want to remove? (enter boat ID): ");
+        int id = sc.nextInt();
+        //Verify given ID
+        while (verifyBoatID(id)) {
+            id = sc.nextInt();
+        }
+        rv_memberRegistry.deleteBoat(id);
+    }
+
+    /*Returns true when boatID is already in file */
+    public boolean verifyBoatID(int boatID){
+        if (rv_memberRegistry.notExistInFile(boatID)) {
+            System.out.print("No such boat! Please try again: ");
+        }
+        return rv_memberRegistry.notExistInFile(boatID);
+    }
+    /* Method translate input to boat  type */
+    public Boat.Type boatTypeView(int a_type){
+        Boat.Type rv_type = null;
+        try {
+            if (a_type == 1)
+                rv_type = Boat.Type.Sailboat;
+            else if(a_type == 2)
+                rv_type = Boat.Type.Motorsailer;
+            else if(a_type == 3)
+                rv_type = Boat.Type.Kayak_Canoe;
+            else if(a_type == 4)
+                rv_type = Boat.Type.Other;
+        } catch (NumberFormatException ex) {
+            System.out.println("\nWrong input! Please try again");
+        }
+        return rv_type;
+    }
+    //
+    //List / Print
 
     /*Method prints compose list of members */
     public void getComposeList() {
@@ -77,7 +218,6 @@ public class RegistryView {
         for (Member m : rv_memberRegistry.members_list)
             printComposeEntry(m);
     }
-
     /* Print one record of compose list in given record */
     public void printComposeEntry(Member a_member){
         int boats = rv_memberRegistry.getNumberOfBoats(a_member);
@@ -86,7 +226,6 @@ public class RegistryView {
         System.out.print(", name: " + a_member.getName());
         System.out.print(", owned boats: " + boats + "\n");
     }
-
     /* Print verbose list with club members */
     public void getVerboseList() {
 
@@ -110,51 +249,5 @@ public class RegistryView {
         System.out.println("BoatID: " + a_boat.getBoatID()
                 + ", boat type: " + a_boat.getBoatType()
                 + ", boat length: " + a_boat.getBoatLength());
-    }
-    /*Returns true when memberID is already in file */
-    public boolean existMemberView(int a_memberID) {
-        if (rv_memberRegistry.notExistInFile(a_memberID)) {
-            System.out.print("No such member! Please try again: ");
-        }
-        return rv_memberRegistry.notExistInFile(a_memberID);
-    }
-    /*Returns true when boatID is already in file */
-    public boolean verifyBoatID(int boatID){
-        if (rv_memberRegistry.notExistInFile(boatID)) {
-            System.out.print("No such boat! Please try again: ");
-        }
-        return rv_memberRegistry.notExistInFile(boatID);
-    }
-    public Boat.Type boatTypeView(int a_type){ //Check this later
-        Boat.Type rv_type = null;
-        try {
-            if (a_type == 1)
-                rv_type = Boat.Type.Sailboat;
-            else if(a_type == 2)
-                rv_type = Boat.Type.Motorsailer;
-            else if(a_type == 3)
-                rv_type = Boat.Type.Kayak_Canoe;
-            else if(a_type == 4)
-                rv_type = Boat.Type.Other;
-        } catch (NumberFormatException ex) {
-            System.out.println("\nWrong input! Please try again");
-        }
-        return rv_type;
-    }
-    public void personalNumberCheck(String a_pn) {
-        Scanner sc = new Scanner(System.in);
-
-        //Check if the input is 10 characters long and if it contains a letter
-        while (!rv_memberRegistry.checkPersonalNumber(a_pn)) {
-            System.out.println("Wrong input try again");
-            a_pn = sc.next();
-        }
-
-        //Check if the input already exist in the file
-        while (!rv_memberRegistry.notExistInFile(Integer.parseInt(a_pn))) {
-            System.out.println("Person number: " + a_pn + " already exist in database");
-            a_pn = sc.next();
-        }
-        sc.close();
     }
 }
